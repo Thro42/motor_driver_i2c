d9f911e847178156f8433b3186dd82e6e220a478
Robert Thomitzek <thro@thomitzek.com>
2020-05-12 21:32:54 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 
+namespace pins {
+     //% block
+    export function MotorRunI2C (chanel: number, speed: number): void {
+        let buf = pins.createBuffer(3);
+    if (speed >= 0) {
+            buf[0] = 2;
+        } else {
+            buf[0] = 3;
+        }
+        buf[1] = chanel;
+        if (speed >= 0) {
+            buf[2] = speed;
+        } else {
+            buf[2] = speed * -1;
+        }
+        pins.i2cWriteBuffer(0x14, buf);
+    }
+}
